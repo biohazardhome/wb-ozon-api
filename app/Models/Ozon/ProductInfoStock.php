f17abf846bedb162857ec05785153c5a9596aa95
biohazardhome <biohazardhome@gmail.com>
2023-03-15 09:29:31 +0300

    Fix cast json

--- app/Models/Ozon/ProductInfoStock.php
+++ app/Models/Ozon/ProductInfoStock.php
@@ -15,21 +15,9 @@ class ProductInfoStock extends Model
             'offer_id',
             'stocks',
         ],
-        $cats = [
+        $casts = [
             'product_id' => 'integer',
             'offer_id' => 'string',
             'stocks' => 'json',
         ];
-
-    public function getStocksAttribute($value) {
-        return $this->fromJson($value, true);
-    }
-
-    public function setStocksAttribute(string|array|object $value) {
-        if (is_string($value)) {
-            $this->attributes['stocks'] = $value;
-        } else if (is_array($value) || is_object($value)) {
-            $this->setJsonCastable('stocks', $value);
-        }
-    }
 }
